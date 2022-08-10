e95a6936f8f3e9ef81819e91be3b29ff37ede462
ignat934@gmail.com <ignat934@gmail.com>
2022-08-10 09:29:16 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
-import ShipsConcurrent.PierLoader;
-import ShipsConcurrent.ShipGenerator;
+
 import ShipsConcurrent.Starter;
-import ShipsConcurrent.Tunnel;
-import ShipsConcurrent.ships.types.Type;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Starter starter = new Starter();
-        starter.start();
+  public static void main(String[] args) throws InterruptedException {
 
 
+  }
 
-    }
+  public static void startFindMinMaxTwoThread() {
+    int[] array = new int[]{1, 20, 3, -5, 1, 5, 0, 12, -24, 17, 12};
 
-    public static void startFindMinMaxTwoThread() {
-        int [] array = new int[]{1,20,3,-5,1, 5, 0, 12, -24, 17, 12};
+    FindMinMaxTwoThreads finder = new FindMinMaxTwoThreads(array);
+    finder.findMinMax();
+  }
 
-        FindMinMaxTwoThreads finder = new FindMinMaxTwoThreads(array);
-        finder.findMinMax();
-    }
+  public static void startShipsConcurrent() {
+    Starter starter = new Starter();
+    starter.start();
+  }
+
+  public static void printStates() throws InterruptedException {
+    ShowStatesOfThreads starter = new ShowStatesOfThreads();
+    starter.startThread();
+  }
+
 }
---
+++ src/ShowStatesOfThreads.java
@@ -0,0 +1,61 @@
+import java.lang.Thread.State;
+
+public class ShowStatesOfThreads {
+
+  private Object lock = new Object();
+
+  Runnable r = new Runnable() {
+    @Override
+    public void run() {
+
+      printState();
+
+      synchronized (lock) {
+        try {
+          lock.notifyAll();
+          lock.wait(3000);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+
+      }
+    }
+  };
+
+  Thread thread = new Thread(r);
+
+  private void printState() {
+    String name = thread.getName();
+    State state = thread.getState();
+    System.out.println("Thread " + name + " is " + state.toString());
+  }
+  public ShowStatesOfThreads() {
+    thread.setName("Max");
+    printState();
+  }
+
+  public void startThread() throws InterruptedException {
+
+    synchronized (lock) {
+      thread.start();
+      lock.wait();
+      printState();
+      lock.notifyAll();
+      printState();
+
+      /*
+      thread.join();
+      System.out.println("Blocked!");
+
+      If print here join with state Blocked, program will stuck here
+
+       */
+
+
+    }
+
+  }
+
+
+
+}
